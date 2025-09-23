81044e020344f931f09353dc2af388adf53f9867
jitendrasinghgurjar <23jitugurjar@gmail.com>
2025-09-23 21:39:33 +0530

    Question 9 pratice

---
+++ JS_Pratice_Task9/script.js
@@ -0,0 +1,5 @@
+let txtarea = document.querySelector('textarea')
+let counter = document.querySelector('#counter')
+txtarea.addEventListener("input",function(){
+    counter.textContent = txtarea.value.length;
+})
